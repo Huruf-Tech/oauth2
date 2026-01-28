63868b6d06ea66d7ae8cefac9b8c4a7ed31a3e9a
Saif Ali Khan <saffellikhan@gmail.com>
2026-01-28 15:17:42 +0500

    chore: add oauth app hook

---
+++ src/hooks/useOauthApp.tsx
@@ -0,0 +1,33 @@
+import { getOauthApp, type TOauthApp } from "@/lib/api";
+import React from "react";
+
+let oauthAppPromise: Promise<TOauthApp>;
+
+export function useOauthApp() {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState<Error | null>(null);
+  const [app, setApp] = React.useState<TOauthApp | null>(null);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    setError(null);
+
+    oauthAppPromise = getOauthApp();
+
+    void (async () => {
+      const app = await oauthAppPromise
+        .catch((error) => {
+          setError(error);
+        })
+        .finally(() => setIsLoading(false));
+
+      if (app) setApp(app);
+    })();
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    app,
+  };
+}
