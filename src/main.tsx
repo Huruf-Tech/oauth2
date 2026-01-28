f407e9e433fbf5f5972461794b4b997e54a3deb0
Saif Ali Khan <mohammadabdullahkhan@mail.com>
2026-01-28 15:03:37 +0500

    chore: router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <App />,
+		},
+	],
+	{
+		basename: import.meta.env.BASE_URL,
+	},
+);
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
