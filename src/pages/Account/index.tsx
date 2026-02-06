8b2236da277ff81eca706f5d9b7ba033afa17c06
Saif Ali Khan <mohammadabdullahkhan@mail.com>
2026-02-06 17:45:37 +0500

    chore: bug fixes and ui updates

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -3,6 +3,8 @@ import { authClient } from "@/lib/auth";
 
 function Account() {
 	const { data, isPending } = authClient.useSession();
+
+	console.log(import.meta.env.BASE_URL);
 	if (!data && !isPending) return <Navigate to="/login" />;
 
 	return <Outlet />;
