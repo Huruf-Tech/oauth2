7c440c3d22ed9354c5077dfdb93eb1c070c115e9
Saif Ali Khan <saffellikhan@gmail.com>
2026-06-12 14:36:13 +0500

    fix: tenant in getMyPolicy hook

--- src/hooks/useMyPolicies.ts
+++ src/hooks/useMyPolicies.ts
@@ -1,21 +1,34 @@
 import React from "react";
 import { ThunderSDK } from "thunder-sdk";
+import { useSearchParams } from "react-router";
 
 type TMyPolicies = Awaited<typeof ThunderSDK.accessControlPolicies.myPolicies>;
 let policies: ReturnType<TMyPolicies>;
 
 export function useMyPolicies() {
+  const [searchParams] = useSearchParams();
+
+  const tenant = searchParams.get("tenant");
+
   const [isLoading, setIsLoading] = React.useState(false);
   const [error, setError] = React.useState<Error | null>(null);
-  const [myPolicies, setMyPolicies] = React.useState<Awaited<
-    typeof policies
-  > | null>(null);
+  const [myPolicies, setMyPolicies] = React.useState<
+    Awaited<
+      typeof policies
+    > | null
+  >(null);
 
   React.useEffect(() => {
     setIsLoading(true);
     setError(null);
 
-    policies ??= ThunderSDK.accessControlPolicies.myPolicies();
+    policies ??= ThunderSDK.accessControlPolicies.myPolicies({
+      axiosConfig: {
+        headers: {
+          "X-TENANT-ID": tenant,
+        },
+      },
+    });
 
     void (async () => {
       const response = await policies
@@ -26,7 +39,7 @@ export function useMyPolicies() {
 
       if (response) setMyPolicies(response);
     })();
-  }, []);
+  }, [tenant]);
 
   return {
     isLoading,
